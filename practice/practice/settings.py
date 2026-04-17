bacff1f22edd7b5da5dd2b18e44bc8903bcc27ee
Makhanov Madiyar <makhanovable@gmail.com>
2026-04-17 05:37:09 +0500

    Update settings.py

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -121,4 +121,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'makhanovable@gmail.com'
-EMAIL_HOST_PASSWORD = 'qyek umsk dhgc ayuo'
+EMAIL_HOST_PASSWORD = 'YOUR KEY'
